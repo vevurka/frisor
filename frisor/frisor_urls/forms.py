a14435551de0270e033a24b1a45e8fd6de61061a
Weronika Sikorska <wmsikorska@gmail.com>
2017-03-25 23:24:02 +0100

    Added forms.py

---
+++ frisor/frisor_urls/forms.py
@@ -0,0 +1,14 @@
+from django import forms
+
+from .models import Url
+
+
+class UrlForm(forms.ModelForm):
+    url = forms.URLField(label='URL')
+    title = forms.CharField(label='Title')
+    nick = forms.CharField(label='Nick')
+
+    class Meta:
+        model = Url
+        fields = ['url', 'title', 'nick']
+
