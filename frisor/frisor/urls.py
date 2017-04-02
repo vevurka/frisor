c7640f3f500bf1f9dfa97cd9245178539a2c4d9c
Weronika Sikorska <wmsikorska@gmail.com>
2017-04-02 23:44:08 +0200

    Added admin.autodiscover for urls in apps

--- frisor/frisor/urls.py
+++ frisor/frisor/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import include, url
 from django.contrib import admin
 
+admin.autodiscover()
+
 urlpatterns = [
     url(r'^', include('frisor_urls.urls')),
     url(r'^admin/', admin.site.urls),
